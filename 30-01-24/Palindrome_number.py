944b0929757b922caf3d19d97af89a801fbdf48d
Kiran <99347320+kiran715@users.noreply.github.com>
2024-01-30 21:20:28 +0530

    Update Palindrome_number.py

--- 30-01-24/Palindrome_number.py
+++ 30-01-24/Palindrome_number.py
@@ -27,7 +27,7 @@ Constraints:
 
 
 
-
+#logic 1
 class Solution(object):
     def isPalindrome(self, x):
         """
@@ -43,8 +43,27 @@ class Solution(object):
                 rem = temp%10
                 tot = tot*10+rem
                 temp//=10
-            return True if tot==x else False
-             
-  
-            
+            return tot == x
+
+
+#logic 2
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x>0: return False
+        div = 1
+        while x >= 10*div:
+            div *= 10
+        while x:
+            left = x // div
+            right = x % 10
+            if left != right:
+                return False
+            x = (x % div)//10
+            div = div / 100
+        return True
+        
         
